e0d9e937ee6896093cb19f0a8ca99391e00fb4f3
demohatesrng <demohatesrng@gmail.com>
2024-01-22 22:38:20 +0530

    painter partition recursion

--- Searching/binary_search_questions_2.cpp
+++ Searching/binary_search_questions_2.cpp
@@ -44,3 +44,30 @@ int Allocation(int arr[], int n, int m){
 }
 
 // Painter partition Problem
+#include <iostream>
+using namespace std;
+int sum(int arr[], int from, int to){
+    int total = 0;
+    for (int i = from; i <= to; i++)
+        total += arr[i];
+    return total;
+}
+// for n boards and k partitions
+int partition(int arr[], int n, int k){
+    // base cases
+    if (k == 1) // one partition
+        return sum(arr, 0, n - 1);
+    if (n == 1) // one board
+        return arr[0];
+    int best = INT_MAX;
+ 
+    for (int i = 1; i <= n; i++)
+        best = min(best, max(partition(arr, i, k - 1), sum(arr, i, n - 1)));
+    return best;
+}
+
+// Aggressive Cows
+#include <iostream>
+using namespace std;
+
+int main(){}
